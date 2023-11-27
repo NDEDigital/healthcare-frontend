b7e2b6e73f3b4932298bad6e001fa46610ef3126
AlamRishad <mahfuzulalamrishad@gmail.com>
2023-11-27 17:15:11 +0600

    slider design updated

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,3 +1,5 @@
-export const API_URL = "https://localhost:7006";
-export const Payment_API_URL = "http://192.168.2.204:96";
- 
+// export const API_URL = "https://localhost:7006";
+// export const Payment_API_URL = "http://192.168.2.204:96";
+
+export const API_URL = 'http://192.168.2.207:91';
+export const Payment_API_URL = 'http://192.168.2.204:96';
