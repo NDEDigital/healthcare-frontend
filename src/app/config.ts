5ce3f79985c5b96858a1cca2dc8ad4cbd218215d
NDEDigital <ndedigitalmarket@gmail.com>
2023-12-03 10:13:05 +0600

    url change to server

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,3 +1,4 @@
-export const API_URL = 'https://localhost:7006';
-//export const API_URL = 'http://192.168.2.207:92';
-export const Payment_API_URL = "http://192.168.2.208:83";
+// export const API_URL = 'https://localhost:7006';
+// export const Payment_API_URL = "http://192.168.2.208:83";
+export const API_URL = "http://172.16.5.18:8775";
+export const Payment_API_URL = "http://192.168.2.207:94";
