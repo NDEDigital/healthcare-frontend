bd61e82d87028099c43307b06e81d6c7af41c451
Toyabur Rahman <toyabur.rahman@ndessl.local>
2023-12-04 18:40:01 +0600

    all the header and title change name with Nimpex HealthCare

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,5 +1,5 @@
 export const API_URL = 'https://localhost:7006';
 
 //export const API_URL = "http://172.16.5.18:8775";
-export const Payment_API_URL = "http://192.168.2.207:94";
-//export const API_URL = 'http://192.168.2.207:92';
+export const Payment_API_URL = "http://localhost:83";
+
