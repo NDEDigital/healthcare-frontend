7f8ccfbaf2a9d265e56f6ea5546aef5bcbc78749
NDEDigital <ndedigitalmarket@gmail.com>
2023-12-06 15:16:15 +0600

    done order

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,5 +1,5 @@
 export const API_URL = 'https://localhost:7006';
 
 //export const API_URL = "http://172.16.5.18:8775";
+// export const Payment_API_URL = "http://localhost:82";
 export const Payment_API_URL = "http://localhost:83";
-
