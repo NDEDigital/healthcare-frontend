1980276fade047fcefee6ce1e971a393d246ab71
Helena Akter Reya <helena.aktar@ndessl.local>
2023-12-27 19:18:46 +0600

    date time added in buyer order

--- src/app/Components/nav-belt/nav-belt.component.ts
+++ src/app/Components/nav-belt/nav-belt.component.ts
@@ -52,16 +52,19 @@ export class NavBeltComponent implements OnInit {
   loadData(): void {
     this.goodsData.getNavData().subscribe((data: any[]) => {
       this.goods = data;
+      console.log(this.goods);
+
       for (let i = 0; i < this.goods.length; i++) {
-        this.products.set(this.goods[i].productGroupCode, this.goods[i].productGroupName);
+        this.products.set(
+          this.goods[i].productGroupCode,
+          this.goods[i].productGroupName
+        );
       }
     });
   }
 
   setSelectData(groupCode: string, groupName: string) {
-
     this.sharedService.setNavSelectData(groupCode, groupName);
-
 
     this.dataUpdated.emit();
     // Update active entry
