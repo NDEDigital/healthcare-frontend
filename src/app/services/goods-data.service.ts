d5af1f380a0959f155536f3e25c97d5d82b41ff9
NDEDigital <ndedigitalmarket@gmail.com>
2024-01-03 17:00:54 +0600

    invoice  added

--- src/app/services/goods-data.service.ts
+++ src/app/services/goods-data.service.ts
@@ -144,16 +144,16 @@ export class GoodsDataService {
 
   // review and ratings
 
-  getReviewRatingsData(goodsId: any, groupCode: string) {
+  getReviewRatingsData(goodsId: any) {
     //console.log(goodsId, groupCode);
 
-    const formData = new FormData();
-    // if (fromDate!=""&& toDate!="" ){
-    formData.append('goodsId', goodsId);
-    formData.append('groupCode', groupCode);
+ 
 
-    const url = `${this.URL}/api/ReviewAndRating/getReviewRatingsData`;
+    const url = `${this.URL}/api/ReviewAndRating/getReviewRatingsDataForDetailsPage`;
     // const url = `${this.baseUrl}/GetOrderData/${pageNumber}/${pageSize}/${status} `;
-    return this.http.post<any>(url, formData);
+ 
+    return this.http.get(url, {
+      params: { goodsId },
+    });
   }
 }
