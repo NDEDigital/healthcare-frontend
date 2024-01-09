42bf56955cc24b9f7f4ee6d37c8a88f37f8c16df
NDEDigital <ndedigitalmarket@gmail.com>
2024-01-09 15:20:48 +0600

    done

--- src/app/Components/product-sidebar/product-sidebar.component.ts
+++ src/app/Components/product-sidebar/product-sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CompanyService } from 'src/app/services/company.service';
 import { GoodsDataService } from 'src/app/services/goods-data.service';
 
 @Component({
@@ -13,19 +14,43 @@ export class ProductSidebarComponent implements OnInit {
   groupData = new Map();
   showAll = false; // Variable to toggle the view state
   initialLimit = 5; // Number of items to show initially
+  // --------------
+  companyStatus:number = 1;
+  companyList:any;
+  showAllBrand = false;
+
   constructor(
-    private goodsData: GoodsDataService,
+    private goodsDataService: GoodsDataService,
+    private companyService:CompanyService
   ) {}
+
   ngOnInit(): void {
-    this.goodsData.getNavData().subscribe((data: any[]) => {
-      console.log(data,"dsdsd");
-      
-     this.goods = data;
-     for (let i = 0; i < this.goods.length; i++) {
-      this.groupData.set(this.goods[i].productGroupCode, this.goods[i].productGroupName);
-    }
+    this.loadCategory();
+    this.loadBrand();
+};
+
+loadCategory():void{
+  this.goodsDataService.getNavData().subscribe((data: any[]) => {
+  
+    
+   this.goods = data;
+   for (let i = 0; i < this.goods.length; i++) {
+    this.groupData.set(this.goods[i].productGroupCode, this.goods[i].productGroupName);
   }
-)};
+});
+}
+loadBrand():void{
+  this.companyService.GetCompaniesBasedOnStatus(this.companyStatus).subscribe({
+    next: (response: any) => {
+      this.companyList = response;
+      console.log(this.companyList," li li");
+      
+    },
+    error: (error: any) => {
+      
+    },
+  });
+}
 
     
 }
