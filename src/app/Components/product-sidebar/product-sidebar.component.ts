ea705c9708276d4164aa099bc3fb78a575f3df96
NDEDigital <ndedigitalmarket@gmail.com>
2024-01-09 13:04:32 +0600

    search sidebar done

--- src/app/Components/product-sidebar/product-sidebar.component.ts
+++ src/app/Components/product-sidebar/product-sidebar.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { GoodsDataService } from 'src/app/services/goods-data.service';
 
 @Component({
   selector: 'app-product-sidebar',
   templateUrl: './product-sidebar.component.html',
   styleUrls: ['./product-sidebar.component.css']
 })
-export class ProductSidebarComponent {
+export class ProductSidebarComponent implements OnInit {
+    
+  goods:any;
+  groupData = new Map();
+  showAll = false; // Variable to toggle the view state
+  initialLimit = 5; // Number of items to show initially
+  constructor(
+    private goodsData: GoodsDataService,
+  ) {}
+  ngOnInit(): void {
+    this.goodsData.getNavData().subscribe((data: any[]) => {
+      console.log(data,"dsdsd");
+      
+     this.goods = data;
+     for (let i = 0; i < this.goods.length; i++) {
+      this.groupData.set(this.goods[i].productGroupCode, this.goods[i].productGroupName);
+    }
+  }
+)};
 
+    
 }
