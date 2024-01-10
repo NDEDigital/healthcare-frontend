9001dc121ddf7fb0b67c5d165df57a53026a7ac9
Helena Akter Reya <helena.aktar@ndessl.local>
2024-01-10 10:48:59 +0600

    user profile and header and subheader redirect bug fixed

--- src/app/Components/sub-header/sub-header.component.ts
+++ src/app/Components/sub-header/sub-header.component.ts
@@ -7,9 +7,34 @@ import { Component, Input } from '@angular/core';
 })
 export class SubHeaderComponent {
   @Input() headerTitle: any;
-  isbuyer = false;
-  ngOnInit() {
+  isBuyer: boolean = false;
+  isAdmin: boolean = false;
+  isSeller: boolean = false;
+  ngOnInit() {}
+  redirect() {
     const role = localStorage.getItem('role');
-    if (role === 'buyer') this.isbuyer = true;
+    if (role === 'admin') {
+      this.isAdmin = true;
+      this.isBuyer = false;
+      this.isSeller = false;
+    }
+    if (role === 'seller') {
+      this.isSeller = true;
+      this.isBuyer = false;
+      this.isAdmin = false;
+    }
+
+    if (role === 'buyer') {
+      this.isBuyer = true;
+      this.isAdmin = false;
+      this.isSeller = false;
+    }
+    console.log(role);
+
+    if (!this.isSeller && !this.isAdmin) {
+      window.location.href = '/';
+    } else {
+      window.location.href = '/dashboard';
+    }
   }
 }
