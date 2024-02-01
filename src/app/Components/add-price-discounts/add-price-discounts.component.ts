6b4f60ba10a45f99d0894b19471c7e8aed92b65a
khaled.jubair <khaled.jubair@STL-SOFT-KJUBAI>
2024-02-01 15:22:48 +0600

    fixed problem of company approval upload images

--- src/app/Components/add-price-discounts/add-price-discounts.component.ts
+++ src/app/Components/add-price-discounts/add-price-discounts.component.ts
@@ -46,7 +46,6 @@ export class AddPriceDiscountsComponent {
   selectedProduct: any;
   allProducts: any[] = [];
   allProductAndGroup: any[] = [];
-  isHovered: any | null = null;
   onProductChange(event: any) {
     const productId = event.target.value;
     console.log(productId, 'productId..');
@@ -132,8 +131,6 @@ export class AddPriceDiscountsComponent {
     this.addPriceDiscountForm
       .get('discountAmount')
       ?.valueChanges.subscribe(() => {});
-
-
 
     this.getProducts(-1);
     this.setupFormValueChanges();
@@ -623,9 +620,7 @@ export class AddPriceDiscountsComponent {
     this.updateFormValidators();
     console.log('product', product);
 
-
     this.getProductData(product.productGroupID);
-
 
     this.populateForm(product);
     this.currentProductPrice = product;
@@ -640,7 +635,7 @@ export class AddPriceDiscountsComponent {
     console.log(product, 'populate form.. ');
 
     console.log('Product ID:', product.productName);
-    console.log(this.products, "products all");
+    console.log(this.products, 'products all');
 
     const isDefaultDate = (date: string) =>
       date.startsWith('0001-01-01T00:00:00');
@@ -664,7 +659,7 @@ export class AddPriceDiscountsComponent {
     this.existingImagePath = product.imagepath;
     this.selectedUnitName = product.unitName;
     this.groupSelect.nativeElement.value = product.productGroupID;
-    this.productSelect.nativeElement.value = product.productId
+    this.productSelect.nativeElement.value = product.productId;
 
     console.log(product.unitName, this.selectedUnitName, 'unit name::');
   }
